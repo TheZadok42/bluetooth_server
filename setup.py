33f51307bdc8f6e00262441d0a37c6a2df11f760
Tal <zadok.tal1@gmail.com>
2021-05-20 13:03:52 +0300

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='bluetooth_server',
-    version='0.0.3',
+    version='0.0.4',
     packages=setuptools.find_packages(),
     install_requires=['PyBluez']
 )
